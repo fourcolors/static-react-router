c3cda9d76721ec9b5106d0f43a5a8dae1e6a6c0a
Sterling Cobb <sterling@inkarnate.com>
2019-10-26 00:05:35 -0700

    Port: port remaining logic in and convert to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,47 @@
 import * as React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import history from './utils/history'
+import { find } from 'ramda'
+import * as UrlPattern from 'url-pattern'
 
-export type RoutesType = {
+export type RouteType = {
   path: String
-  component: React.Component
+  component: React.SFC
   check: Function
   redirect: String
 }
 
 export type RouterPropType = {
-  routes: RoutesType
-  NotFound: React.Component
+  routes: RouteType[]
+  NotFound: React.SFC
 }
 
 export const Router: React.SFC<RouterPropType> = ({ routes, NotFound }) => {
   const [location, setLocation] = useState(history.location)
-  return <div />
+  useEffect(() => {
+    const unlisten = history.listen((location) => {
+      setLocation(location)
+    })
+    return () => {
+      unlisten()
+    }
+  })
+
+  let params
+  const route = find((route: RouteType) => {
+    const pattern = new UrlPattern(route.path as string)
+    return (params = pattern.match(location.pathname))
+  }, routes)
+
+  if (!route) {
+    return <NotFound />
+  }
+
+  if (route.check && route.check()) {
+    history.push(route.redirect as string)
+    history.go(1)
+    return null
+  }
+
+  return <route.component as React {...params} />
 }
